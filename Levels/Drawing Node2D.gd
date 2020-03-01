33363b543c3f40db09024df6eccebc46aaa955ea
mariothedog <mariothedogmc@gmail.com>
2020-03-01 15:44:45 +0000

    Connected button to the door (still just visual)

---
+++ Levels/Drawing Node2D.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _process(_delta):
+	update()
+
+func _draw():
+	for button in get_tree().get_nodes_in_group("Buttons"):
+		var button_lowest_pos = button.position + Vector2(0, 40)
+		var connected_lowest_pos = button_lowest_pos + Vector2((button.connected_node.position - button.position).x, 0)
+		draw_line(button.position, button_lowest_pos, Color.red, 5, true)
+		draw_line(button_lowest_pos, connected_lowest_pos, Color.red, 5, true)
+		draw_line(connected_lowest_pos, button.connected_node.position, Color.red, 5, true)
--- Levels/Level.gd
+++ Levels/Level.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 func _ready():
 	$ColorRect.color = Color(0.57, 0.66, 0.73);
