1baa9a664ff0fe9ecaebe0546af95c7eef940a8f
mariothedog <mariothedogmc@gmail.com>
2020-03-04 16:34:02 +0000

    Wire colour now changes when the button is pushed

--- Levels/Drawing Node2D.gd
+++ Levels/Drawing Node2D.gd
@@ -7,6 +7,11 @@ func _draw():
 	for button in get_tree().get_nodes_in_group("Buttons"):
 		var button_lowest_pos = button.position + Vector2(0, 40)
 		var connected_lowest_pos = button_lowest_pos + Vector2((button.connected_node.position - button.position).x, 0)
-		draw_line(button.position, button_lowest_pos, Color.red, 5, true)
-		draw_line(button_lowest_pos, connected_lowest_pos, Color.red, 5, true)
-		draw_line(connected_lowest_pos, button.connected_node.position + button.connected_node.wire_offset, Color.red, 5, true)
+		
+		var colour = Color.gray
+		if button.pushed:
+			colour = Color(1, 1, 0.13)
+		
+		draw_line(button.position, button_lowest_pos, colour, 5, true)
+		draw_line(button_lowest_pos, connected_lowest_pos, colour, 5, true)
+		draw_line(connected_lowest_pos, button.connected_node.position + button.connected_node.wire_offset, colour, 5, true)
--- Objects/Button.gd
+++ Objects/Button.gd
@@ -5,6 +5,8 @@ onready var connected_node = get_node(connected_node_path)
 
 var velocity = Vector2()
 
+var pushed = false
+
 func _physics_process(delta):
 	velocity.y += global.GRAVITY * delta
 	
@@ -14,8 +16,12 @@ func _on_Press_Detection_body_entered(_body):
 	$Tween.interpolate_property($"Pushable Button", "position", Vector2.ZERO, Vector2(0, 10), 0.1)
 	$Tween.start()
 	
+	pushed = true
+	
 	get_node(connected_node_path).activate()
 
 func _on_Press_Detection_body_exited(_body):
 	$Tween.interpolate_property($"Pushable Button", "position", $"Pushable Button".position, Vector2.ZERO, 0.1)
 	$Tween.start()
+	
+	pushed = false
