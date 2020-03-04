eea297f3e472823e33c65b992ab8b55aa3f64959
mariothedog <mariothedogmc@gmail.com>
2020-03-04 16:37:19 +0000

    Button can no longer be pushed while it is already pushed

--- Objects/Button.gd
+++ Objects/Button.gd
@@ -13,12 +13,13 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _on_Press_Detection_body_entered(_body):
-	$Tween.interpolate_property($"Pushable Button", "position", Vector2.ZERO, Vector2(0, 10), 0.1)
-	$Tween.start()
-	
-	pushed = true
-	
-	get_node(connected_node_path).activate()
+	if not pushed:
+		$Tween.interpolate_property($"Pushable Button", "position", Vector2.ZERO, Vector2(0, 10), 0.1)
+		$Tween.start()
+		
+		pushed = true
+		
+		get_node(connected_node_path).activate()
 
 func _on_Press_Detection_body_exited(_body):
 	$Tween.interpolate_property($"Pushable Button", "position", $"Pushable Button".position, Vector2.ZERO, 0.1)
