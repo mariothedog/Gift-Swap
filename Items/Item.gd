754559fe40e46ac24819b7210f09dfc9e2efc48a
mariothedog <mariothedogmc@gmail.com>
2020-03-07 13:45:09 +0000

    Added physics to the bouncy ball

--- Items/Item.gd
+++ Items/Item.gd
@@ -2,8 +2,19 @@ extends RigidBody2D
 
 export (String, "Book", "Blue Book", "Bouncy Ball") var type
 
+onready var item_properties = {
+	"Bouncy Ball" : [1, false, 0.8, false]
+}
+
 func _ready():
 	$Sprite.texture = global.item_textures[type]
+	
+	var properties = item_properties.get(type)
+	if properties:
+		physics_material_override.friction = properties[0]
+		physics_material_override.rough = properties[1]
+		physics_material_override.bounce = properties[2]
+		physics_material_override.absorbent = properties[3]
 
 func _on_Area2D_body_entered(body):
 	queue_free()
