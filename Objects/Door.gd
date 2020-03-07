1a18e17c3613b7e57e5f3b84f634f271d8b91af0
mariothedog <mariothedogmc@gmail.com>
2020-03-07 08:56:44 +0000

    Improved the door slide animation

--- Objects/Door.gd
+++ Objects/Door.gd
@@ -6,11 +6,13 @@ onready var open_vector = Vector2(64, 120)
 onready var closed_vector = Vector2(64, 15)
 
 func activate():
-	$"Slide Tween".interpolate_method(self, "_tween_region_rect", $Sprite.region_rect.size, closed_vector, 0.8)
+	$"Slide Tween".interpolate_method(self, "_tween_region_rect", $Sprite.region_rect.size, closed_vector,
+	0.8, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	$"Slide Tween".start()
 
 func unactivate():
-	$"Slide Tween".interpolate_method(self, "_tween_region_rect", $Sprite.region_rect.size, open_vector, 0.8)
+	$"Slide Tween".interpolate_method(self, "_tween_region_rect", $Sprite.region_rect.size, open_vector,
+	0.8, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	$"Slide Tween".start()
 
 func _tween_region_rect(size):
