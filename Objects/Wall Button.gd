0db968b8a4d48b11f8a07259080c2d8337a03f92
mariothedog <mariothedogmc@gmail.com>
2020-03-08 09:39:25 +0000

    Improved the wall button push effect

--- Objects/Wall Button.gd
+++ Objects/Wall Button.gd
@@ -9,20 +9,22 @@ var not_pressing_timer = 0
 
 var modulate_rgb = Vector3(1, 1, 1)
 
+var pushed_colour = 0.9
+
 func _physics_process(delta):
 	if $"Press Detection".get_overlapping_bodies():
 		not_pressing_timer = 0.21
-		modulate_rgb = modulate_rgb.move_toward(Vector3(0, 0, 0), 0.02)
+		modulate_rgb = modulate_rgb.move_toward(Vector3(pushed_colour, pushed_colour, pushed_colour), 0.02)
 		update_modulate()
 	else:
 		if not_pressing_timer >= 0:
-			modulate_rgb = modulate_rgb.move_toward(Vector3(0, 0, 0), 0.02)
+			modulate_rgb = modulate_rgb.move_toward(Vector3(pushed_colour, pushed_colour, pushed_colour), 0.02)
 			update_modulate()
 		else:
 			modulate_rgb = modulate_rgb.move_toward(Vector3(1, 1, 1), 0.02)
 			update_modulate()
 	
-	if modulate_rgb.x <= 0.7:
+	if modulate_rgb.x <= pushed_colour:
 		if not pushed:
 			pushed = true
 			get_node(connected_node_path).activate()
