cda3841b70fa115efd3763de36e312679b12c1ea
mariothedog <mariothedogmc@gmail.com>
2020-03-20 15:53:56 +0000

    Removed the unused shader materials on the other levels and moved wire drawing to the main level node

--- Levels/Level.gd
+++ Levels/Level.gd
@@ -7,6 +7,9 @@ func _ready():
 	0.4, Tween.TRANS_SINE, Tween.EASE_OUT)
 	$"Level Transition".start()
 
+func _process(_delta):
+	update()
+
 func next_level():
 	global.current_level += 1
 	
@@ -17,3 +20,16 @@ func next_level():
 	yield($"Level Transition", "tween_completed")
 	if get_tree().change_scene("res://Levels/Level%s.tscn" % global.current_level) != OK:
 		print_debug("An error occured while changing scene.")
+
+func _draw():
+	for button in get_tree().get_nodes_in_group("Buttons"):
+		var button_lowest_pos = button.position + Vector2(0, 40)
+		var connected_lowest_pos = button_lowest_pos + Vector2((button.connected_node.position - button.position).x, 0)
+		
+		var colour = Color.gray
+		if button.pushed:
+			colour = Color(1, 1, 0.13)
+		
+		draw_line(button.position, button_lowest_pos, colour, 5, true)
+		draw_line(button_lowest_pos, connected_lowest_pos, colour, 5, true)
+		draw_line(connected_lowest_pos, button.connected_node.position + button.connected_node.wire_offset, colour, 5, true)
